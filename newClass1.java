bb72fdb581ca4b9531aa23dee8c6a5c471f93091
Jose Miguel Garcia <jmgbenayas@gmail.com>
2015-10-26 16:24:53 +0100

    #2 - añadidos archivos iniciales

---
+++ newClass1.java
@@ -0,0 +1,20 @@
+/*
+ **********************************************
+ * @author: Jose Miguel Garcia Benayas.
+ * @email: jm.garciaben@urjc.es
+ **********************************************
+ * @version: 26-oct-2015 - 16:16:28 .
+ **********************************************
+ */
+public class newClass1 {
+    //Atributtes
+    private 
+    
+    //Constructors
+    public newClass1(){
+        
+}
+
+    //Methods
+    
+}
